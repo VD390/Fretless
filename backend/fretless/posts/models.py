6b3f6064747f900a11d1c9816b49e30fcc61525b
Vasilii Maltsev <super.umis@yandex.ru>
2023-09-18 19:14:29 +0500

    создание моделей Group, Tag, Post, Comment, Follow, Favorite.

--- backend/fretless/posts/models.py
+++ backend/fretless/posts/models.py
@@ -1,3 +1,159 @@
 from django.db import models
+from fretless.settings import FIRST_CHAR_STRING
 
-# Create your models here.
+from users.models import CustomUser
+from validators import validator_color
+
+
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True,
+        verbose_name="Название",
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name="Слаг",
+    )
+    description = models.TextField(max_length=200)
+
+    class Meta:
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
+        ordering = ['title']
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        verbose_name='Слаг'
+    )
+    color = models.CharField(
+        max_length=7,
+        null=True,
+        validators=[validator_color],
+        verbose_name='Цвет'
+    )
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    text = models.TextField(
+        verbose_name='Текст поста'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата публикации")
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts'
+    )
+    image = models.ImageField(
+        verbose_name='Картинка',
+        upload_to='posts/',
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text[:FIRST_CHAR_STRING]
+
+# Сделать у поста Тэг или группу?
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    text = models.TextField(
+        verbose_name='Текст комментария',
+        help_text='Введите комментарий'
+    )
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.text[:FIRST_CHAR_STRING]
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Пользователь',
+    )
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор'
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        unique_together = ('user', 'author')
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='favorite',
+        verbose_name='Пользователь'
+    )
+    post = models.ForeignKey(
+        CustomUser,
+        on_delete=models.ForeignKey,
+        related_name='favorite',
+        verbose_name='Рецепт'
+    )
+
+    class Meta:
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранные'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'post'],
+                name='unique_user_posts'
+            )
+        ]
